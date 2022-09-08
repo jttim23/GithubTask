0f5681a6a70c366a41d6a00d8b76995fcf39b98a
jedro <jttim23@gmail.com>
2022-09-08 16:51:22 +0200

    initial

---
+++ src/test/java/com/task/githubTask/GithubTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.githubTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
